7780cd5209275cfa8f6f128bca5a9850ce29d4fe
Adelson Barros Dos Santos <40273131+adelson101@users.noreply.github.com>
2024-03-17 14:08:20 -0300

    up

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -15,9 +15,7 @@ export class LivroService {
   buscar(valorDigitado: string): Observable<Item[]>{
     const params = new HttpParams().append('q', valorDigitado);
     return this.http.get<LivrosResultado>(this.API, { params } ).pipe(
-      tap((RETORNOAPI)=> console.log('fluxo do tap',RETORNOAPI)),
       map(resultadoAPI => resultadoAPI.items),
-      tap(resultado => console.log('fluxo após o map',resultado))
     );
   }
 }
